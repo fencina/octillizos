75b4cf34cc65d3153ec82e5fe8fa331f56134d73
fencina-bondacom <fencina@bondacom.com>
2017-05-15 22:16:07 -0300

    Proccess all clues

--- src/main/java/ar/edu/utn/frba/ia/ag/ejemplos/perfumes/CombinacionPerfumes.java
+++ src/main/java/ar/edu/utn/frba/ia/ag/ejemplos/perfumes/CombinacionPerfumes.java
@@ -2,7 +2,7 @@ package main.java.ar.edu.utn.frba.ia.ag.ejemplos.perfumes;
 
 import main.java.ar.edu.utn.frba.ia.ag.Individuo;
 import java.util.concurrent.ThreadLocalRandom;
-import main.java.ar.edu.utn.frba.ia.ag.ejemplos.perfumes.pistas.PistaUno;
+import main.java.ar.edu.utn.frba.ia.ag.ejemplos.perfumes.pistas.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.logging.Logger;
@@ -24,15 +24,34 @@ public class CombinacionPerfumes extends Individuo {
 	@Override
 	public double aptitud() {
 
-		// TODO - Crear todas las pistas e ir acumulando la aptitud segun la resolucion de cada una
-		// aptitud = 0;
-		// foreach( pistas as pista) {
-		//		aptitud += pista.resolver(); }
-		// return aptitud;
+		double valorAptitud = 0;
 
-		PistaUno pista = new PistaUno(getPerfumes());
-		return pista.resolver();
+		ArrayList<Pista> pistas = getPistas();
+
+		for (Pista pista : pistas ) {
+			valorAptitud += pista.resolver();
+		}
+
+		return valorAptitud;
 	}
+
+    public ArrayList<Pista> getPistas() {
+
+       ArrayList<Pista> pistas = new ArrayList<Pista>();
+
+       pistas.add(new PistaUno(getPerfumes()));
+       pistas.add(new PistaDos(getPerfumes()));
+       pistas.add(new PistaTres(getPerfumes()));
+       pistas.add(new PistaCuatro(getPerfumes()));
+       pistas.add(new PistaCinco(getPerfumes()));
+       pistas.add(new PistaSeis(getPerfumes()));
+       pistas.add(new PistaSiete(getPerfumes()));
+       pistas.add(new PistaOcho(getPerfumes()));
+       pistas.add(new PistaNueve(getPerfumes()));
+
+       return pistas;
+    }
+
 
 	public ArrayList<Perfume> getPerfumes() {
 		return perfumes;
