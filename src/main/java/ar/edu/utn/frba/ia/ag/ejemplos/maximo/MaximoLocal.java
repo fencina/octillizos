d0dc4e3b779b10b069202fa7f4e6c0925839204b
augustobonabia <augusto.bonabia@hotmail.com>
2017-05-20 02:50:56 -0300

    configuracions

--- src/main/java/ar/edu/utn/frba/ia/ag/ejemplos/maximo/MaximoLocal.java
+++ src/main/java/ar/edu/utn/frba/ia/ag/ejemplos/maximo/MaximoLocal.java
@@ -5,9 +5,12 @@ import java.util.logging.Logger;
 import main.java.ar.edu.utn.frba.ia.ag.AlgoritmoGenetico;
 import main.java.ar.edu.utn.frba.ia.ag.ConfiguracionDefault;
 import main.java.ar.edu.utn.frba.ia.ag.Individuo;
+import main.java.ar.edu.utn.frba.ia.ag.cruzamiento.BinomialAzar;
 import main.java.ar.edu.utn.frba.ia.ag.cruzamiento.BinomialMascaraComplemento;
 import main.java.ar.edu.utn.frba.ia.ag.mutacion.MutacionSimple;
+import main.java.ar.edu.utn.frba.ia.ag.paro.TiempoTranscurrido;
 import main.java.ar.edu.utn.frba.ia.ag.seleccion.ControlSobreNumeroEsperado;
+import main.java.ar.edu.utn.frba.ia.ag.seleccion.Ranking;
 
 public class MaximoLocal {
 	
@@ -24,6 +27,11 @@ public class MaximoLocal {
 //		new Torneo(), // seleccion
 //		new Simple(), // cruzamiento
 //		new MutacionSimple(0.2)
+		c.setCriterioDeParo(new TiempoTranscurrido(0, 1, 0));
+		c.setMetodoDeSeleccion(new Ranking(30));
+		c.setPoblacionInicial(99999);
+		c.setCruzamiento(new BinomialAzar());
+		c.setMutacion(new MutacionSimple(0));
 		
 		AlgoritmoGenetico maximoLocal = new AlgoritmoGenetico(c, X.class);
 		
--- src/main/java/ar/edu/utn/frba/ia/ag/ejemplos/perfumes/AcertijoPerfumes.java
+++ src/main/java/ar/edu/utn/frba/ia/ag/ejemplos/perfumes/AcertijoPerfumes.java
@@ -1,9 +1,14 @@
 package main.java.ar.edu.utn.frba.ia.ag.ejemplos.perfumes;
 
+import java.util.List;
+
 import main.java.ar.edu.utn.frba.ia.ag.AlgoritmoGenetico;
 import main.java.ar.edu.utn.frba.ia.ag.Configuracion;
 import main.java.ar.edu.utn.frba.ia.ag.ConfiguracionDefault;
+import main.java.ar.edu.utn.frba.ia.ag.Estado;
+import main.java.ar.edu.utn.frba.ia.ag.Individuo;
 import main.java.ar.edu.utn.frba.ia.ag.cruzamiento.*;
+import main.java.ar.edu.utn.frba.ia.ag.mutacion.*;
 import main.java.ar.edu.utn.frba.ia.ag.paro.*;
 import main.java.ar.edu.utn.frba.ia.ag.seleccion.*;
 
@@ -16,9 +21,9 @@ public class AcertijoPerfumes {
 		
 		//Descomentar para tener la "configuracion 2"
 		{
-		config.setCriterioDeParo(new TiempoTranscurrido(0, 1, 0));
-		config.setMetodoDeSeleccion(new Ranking(100));
-		config.setPoblacionInicial(500000);
+//		config.setCriterioDeParo(new TiempoTranscurrido(0, 1, 0));
+//		config.setMetodoDeSeleccion(new Ranking(100));
+//		config.setPoblacionInicial(500000);
 		}
 		//Descomentar junto con la "configuracion 2" para tener la "configuracion 3"
 		{
@@ -26,10 +31,11 @@ public class AcertijoPerfumes {
 		}
 		//Otra configuracion
 		{
-//			config.setCriterioDeParo(new TiempoTranscurrido(0, 5, 0));
-//			config.setMetodoDeSeleccion(new Ranking(30));
-//			config.setPoblacionInicial(99999);
-//			config.setCruzamiento(new BinomialAzar());
+			config.setCriterioDeParo(new TiempoTranscurrido(0, 15, 0));
+			config.setMetodoDeSeleccion(new Ranking(30));
+			config.setPoblacionInicial(99999);
+			config.setCruzamiento(new BinomialAzar());
+			config.setMutacion(new MutacionSimple(0.1));
 		}
 		
 		AlgoritmoGenetico combinacionPerfumes = new AlgoritmoGenetico(config, CombinacionPerfumes.class);
